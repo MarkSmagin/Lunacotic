5c52df2be5a41190b47defdab8caa1e5d151fa9a
Mark Smagin <lifeindeadcity@gmail.com>
2022-09-12 13:56:28 +0300

    Сверстаны Главная страница, страница Каталога для кошек, страница Каталога для кошек с товарами

--- scripts/script.js
+++ scripts/script.js
@@ -31,26 +31,55 @@ $(document).ready(function(){
     heightStyle: 'content',
     header: '> .questions__item > .question__text'
   });
-  // Карта
-  ymaps.ready(init);
-  function init(){
-      let myMap = new ymaps.Map("map", {
-          center: [55.795798, 49.222449],
-          zoom: 15,
-          controls: ['zoomControl']
-      },
-      {
-          suppressMapOpenBlock: true
+   // Аккордион фильтров
+  $('.products__filters').accordion({
+    heightStyle: 'content',
+    active: true,
+    header: '> .filter__accordion > .filter__name',
+    collapsible: true,
+    beforeActivate: function(event, ui) {
+      if (ui.newHeader[0]) {
+        var currHeader  = ui.newHeader;
+        var currContent = currHeader.next('.ui-accordion-content');
+      } else {
+        var currHeader  = ui.oldHeader;
+        var currContent = currHeader.next('.ui-accordion-content');
       }
-      );
-      let myPlacemark = new ymaps.Placemark([55.795798, 49.222449], {
-        hintContent: 'Казань, Мамадышский тракт 4',
-      }, {
-          iconLayout: 'default#image',
-          iconImageHref: 'images/map__marker.svg',
-          iconImageSize: [48, 48],
-    });
-    myMap.geoObjects.add(myPlacemark);
+      var isPanelSelected = currHeader.attr('aria-selected') == 'true';
+      currHeader.toggleClass('ui-corner-all',isPanelSelected).toggleClass('accordion-header-active ui-state-active ui-corner-top',!isPanelSelected).attr('aria-selected',((!isPanelSelected).toString()));
+      currHeader.children('.ui-icon').toggleClass('ui-icon-triangle-1-e',isPanelSelected).toggleClass('ui-icon-triangle-1-s',!isPanelSelected);
+      currContent.toggleClass('accordion-content-active',!isPanelSelected)    
+      if (isPanelSelected) { currContent.slideUp(); }  else { currContent.slideDown(); }
+      return false;
+    },
+    animate: {
+      duration: 100
+    }
+  });
+
+
+  // Карта
+  if($('#map').length){
+    ymaps.ready(init);
+    function init(){
+        let myMap = new ymaps.Map("map", {
+            center: [55.795798, 49.222449],
+            zoom: 15,
+            controls: ['zoomControl']
+        },
+        {
+            suppressMapOpenBlock: true
+        }
+        );
+        let myPlacemark = new ymaps.Placemark([55.795798, 49.222449], {
+          hintContent: 'Казань, Мамадышский тракт 4',
+        }, {
+            iconLayout: 'default#image',
+            iconImageHref: 'images/map__marker.svg',
+            iconImageSize: [48, 48],
+      });
+      myMap.geoObjects.add(myPlacemark);
+    }
   }
   // Боковые кнопки
   $(window).scroll(function() {
@@ -76,19 +105,83 @@ $(document).ready(function(){
   )
   $('.product_slider__like img').on('click', function() {
     if($(this).hasClass('active')){
-      console.log('имеет')
       $(this).removeClass('active');
       $(this).attr('src', 'images/like.svg');
     } else {
-      console.log('не имеет')
       $(this).addClass('active');
       $(this).attr('src', 'images/like__active.svg');
     }
   })
-  // Горизонтальная прокрутка
-  const swiper = new Swiper('.swiper', {
-    slidesPerView: 12,
-    spaceBetween: 10,
-    freeMode: true,
+
+    // Горизонтальная прокрутка
+  if($('.swiper').length){
+    const swiper = new Swiper('.swiper', {
+      slidesPerView: 12,
+      spaceBetween: 10,
+      freeMode: true,
+    });
+  }
+  // Ползунок фильтр цен
+  $(".polzunok-5").slider({
+    min: 0,
+    max: 18000,
+    values: [0, 18000],
+    range: true,
+    animate: "fast",
+    slide : function(event, ui) {    
+        $(".polzunok-input-5-left").val(ui.values[ 0 ]);   
+        $(".polzunok-input-5-right").val(ui.values[ 1 ]);  
+    }    
+  });
+  $('.polzunok-input-wrap input').focus(function(){
+    $(this).val('');
+  });
+
+  // Автозаполнение полей ползунка **На доработку
+  // $('.polzunok-input-wrap input').focusout(function(){
+  //   console.log($(this).val())
+  //   if(!$(this).val()){
+  //     if($(this).hasClass('polzunok-input-5-left')){
+  //       $(this).val('0');
+  //     } else {
+  //       $(this).val('18000');
+  //     }
+  //   }
+  // });
+  $(".polzunok-input-5-left").val($(".polzunok-5").slider("values", 0));
+  $(".polzunok-input-5-right").val($(".polzunok-5").slider("values", 1));
+  $(".polzunok-container-5 input").change(function() {
+    var input_left = $(".polzunok-input-5-left").val().replace(/[^0-9]/g, ''),    
+    opt_left = $(".polzunok-5").slider("option", "min"),
+    where_right = $(".polzunok-5").slider("values", 1),
+    input_right = $(".polzunok-input-5-right").val().replace(/[^0-9]/g, ''),    
+    opt_right = $(".polzunok-5").slider("option", "max"),
+    where_left = $(".polzunok-5").slider("values", 0); 
+    if (input_left > where_right) { 
+        input_left = where_right; 
+    }
+    if (input_left < opt_left) {
+        input_left = opt_left; 
+    }
+    if (input_left == "") {
+    input_left = 0;    
+    }        
+    if (input_right < where_left) { 
+        input_right = where_left; 
+    }
+    if (input_right > opt_right) {
+        input_right = opt_right; 
+    }
+    if (input_right == "") {
+    input_right = 0;    
+    }    
+    $(".polzunok-input-5-left").val(input_left); 
+    $(".polzunok-input-5-right").val(input_right); 
+    if (input_left != where_left) {
+        $(".polzunok-5").slider("values", 0, input_left);
+    }
+    if (input_right != where_right) {
+        $(".polzunok-5").slider("values", 1, input_right);
+    }
   });
 })
